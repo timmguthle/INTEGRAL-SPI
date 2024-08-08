21d7117c301709282831d03fb815254bbe48aa3a
Timm Güthle <timm.guethle@gmx.de>
2024-08-08 10:33:09 +0200

    minor change

--- main_files/crab_19/crab_fits.py
+++ main_files/crab_19/crab_fits.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.insert(0, os.path.abspath('./main_files'))
 
 import numpy as np
-from MultinestClusterFit_with_psd_eff import MultinestClusterFit
+from MultinestClusterFit import MultinestClusterFit
 from RebinningFunctions import spimodfit_binning_SE, log_binning_function_for_x_number_of_bins, no_rebinning #, rebin_data_exp_50
 from PointingClusters import *
 from ModelSources import *
